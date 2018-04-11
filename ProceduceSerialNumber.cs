ec690a76d102ffc6c2e0db947ccef111cfe1f67d
anonymoux <405688660@qq.com>
2018-04-11 17:31:35 +0800

    modify notes

--- ProceduceSerialNumber.cs
+++ ProceduceSerialNumber.cs
@@ -6,7 +6,7 @@ public string ProceduceSerialNumber()
 	string sql = "SELECT column_name FROM table_name WHERE id LIKE '"+code+"%"+"' ORDER BY column_id DESC";
 	//the result of search collection assigns to dt
 	DataTable dt = this.GetDataTable(sql);
-	//jurge id have been proceduce serial number, if not, the serial number begin as 001
+	//jurge id if it have been proceduce serial number, if no, the serial number begin as 001
 	if(dt.Rows.Count == 0) return code + "001";
 	string id = dt.Rows[0]["column_id"].ToString().Replace(code,"").Trim();
 	code = code + (Convert.ToInt16(id) + 1).ToString().PadLeft(3,'0'); //generate serial number xxx(three-digit number)
